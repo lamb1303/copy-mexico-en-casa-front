103555c76388b9fa979b48ba3baee15951331899
IrvinGalindo <irvin_its@outlook.es>
2020-04-23 23:58:09 -0500

    feat(global style): Add global colors

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Home from './components/Home/Home';
 import * as serviceWorker from './serviceWorker';
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import registroReducer from './store/reducers/registro';
-import clienteReducer from './store/reducers/cliente';
+//import clienteReducer from './store/reducers/cliente';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
@@ -14,7 +15,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const rootReducer = combineReducers({
   registro: registroReducer,
-  cliente: clienteReducer
+//  cliente: clienteReducer
 });
 
 const store = createStore(rootReducer, composeEnhancers(
@@ -24,7 +25,7 @@ const store = createStore(rootReducer, composeEnhancers(
 const app = (
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+        <Home />
     </BrowserRouter>
   </Provider>
 )
