bd8688d62aee34f0fc97b66abc07ec26f96956bb
Bladimir <oirbl@hotmail.com>
2020-04-28 20:22:18 -0500

    add client and business register

--- src/store/actions/cliente.js
+++ src/store/actions/cliente.js
@@ -1,40 +1,6 @@
 import * as actionTypes from './actionTypes';
 import axios from 'axios';
 
-// export const crearCuenta = () => {
-//     return {
-//         type: actionTypes.CLIENTE_CREAR_CUENTA
-//     }
-// };
-
-// export const iniciarRequestCreacion = () => {
-//     return {
-//         type: actionTypes.CLIENTE_INICIAR_CREACION
-//     }
-// }
-
-// export const falloLaCreacion = () => {
-//     return {
-//         type: actionTypes.CLIENTE_FALLO_CREACION_CUENTA
-//     }
-// }
-
-
-// export const crearCuentaCliente = (datos) => {
-//     return dispatch => {
-
-//         dispatch(iniciarRequestCreacion());
-//         axios.post('/laurl/api/crearUsuario', datos)
-//             .then(response => {
-//                 if (response.data.status === 201) {
-//                     dispatch(crearCuenta());
-//                 } else {
-//                     dispatch(falloLaCreacion())
-//                 }
-//             })
-//     }
-// }
-
 export const OpenSelectedProduct = (product) => {
     return {
         type: actionTypes.OPEN_ADD_DEL_OPTIONS,
