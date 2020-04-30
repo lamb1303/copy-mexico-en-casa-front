e0a36a7ae4c199cd90dc37d411a79e5dee3f15e1
Edson Davila <edson117ful@gmail.com>
2020-04-30 00:25:30 -0500

    add last validations

--- src/components/UI/Header/Header.js
+++ src/components/UI/Header/Header.js
@@ -6,10 +6,17 @@ import * as actions from '../../../store/actions';
 
 const Header = props => {
 
+    let displayB = false
+    let space = ""
+    if (window.location.pathname === 'Registro' || window.location.pathname === 'Registro' ) {
+        displayB = true
+        space = " space"
+    }
+
     return (
         <Fragment>
             <div className={classes.header}>
-                <Burguer className={classes.header_burguer} onClick={() => props.onClickBurguer()} />
+                {displayB && <Burguer className={classes.header_burguer} onClick={() => props.onClickBurguer()} />}
                 <div className={classes.header_title}>MÉXICO EN CASA</div>
                 <img className={classes.header_logo} alt='Mexico En Casa Logo' src='../logo.png' />
             </div>
