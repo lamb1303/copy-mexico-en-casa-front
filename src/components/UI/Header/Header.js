63e434992d18f816c06ba186e83c052bc54da8ff
Unknown <oirbl@hotmail.com>
2020-05-08 12:37:35 -0500

    merge fixes

--- src/components/UI/Header/Header.js
+++ src/components/UI/Header/Header.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react';
 import { useLocation } from 'react-router-dom';
 import classes from './Header.module.scss';
 import { ReactComponent as Burguer } from './burguer.svg';
+import { ReactComponent as Logo } from './burguer.svg';
 import { connect } from 'react-redux';
 import * as actions from '../../../store/actions';
 
@@ -19,7 +20,7 @@ const Header = props => {
 
     return (
         <Fragment>
-            {!displayH && <div className={classes.header}>
+            {!displayH && <div className={[classes.header, classes[space]].join(' ')}>
                 {displayB && <Burguer className={classes.header_burguer} onClick={() => props.onClickBurguer()} />}
                 <div className={classes.header_title}>MÉXICO EN CASA</div>
                 <img className={classes.header_logo} alt='Mexico En Casa Logo' src='../logo.png' />
